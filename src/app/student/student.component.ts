3cf92deaf07a3116d0235166e21d044cc2c0b04e
tajleena <tajleena15@gmail.com>
2020-01-29 12:09:32 +0530

    3 files modified

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -7,10 +7,10 @@ import {Router} from '@angular/router';
 })
 export class StudentComponent implements OnInit {
 students=[
-  {id:1,name:"Dipti",emailId:"dipti@gmail.com",password:"1234"},
-{id:2,name:"leena",emailId:"leena@gmail.com",password:"1234"},
+  {id:1,name:"Dipti",emailId:"dipti@gmail.com",department:"Engineering",password:"1234"},
+{id:2,name:"leena",emailId:"leena@gmail.com", department:"Deep Studies",password:"1234"},
 
-{id:3,name:"stuti",emailId:"stuti@gmail.com",password:"1234"}
+{id:3,name:"stuti",emailId:"stuti@gmail.com", department:"Arts",password:"1234"}
 ]
 constructor(private router:Router){
 
